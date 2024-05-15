730efbd2d9454143edf1e4d4fd6120515ae7ebc3
4rwill <christian10will@gmail.com>
2024-05-15 16:30:02 -0300

    Criando projeto para tipos e variáveis

--- dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
+++ dio-trilha-java-basico/tipos-variaveis/src/TiposVariaveis.java
@@ -1,18 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception{
-        double salarioMinimo = 2500;
 
+        double salarioMinimo = 2500;
         short numeroCurto = 1;
         int numeroNormal = numeroCurto;
         short numeroCurto2 = (short) numeroNormal;
-
         int numero = 1;
-
-        numero = 12;
-
-
-    final double VALOR_DE_PI = 3.14;
-
+        final double VALOR_DE_PI = 3.14;
 
         System.out.print(VALOR_DE_PI);
 
