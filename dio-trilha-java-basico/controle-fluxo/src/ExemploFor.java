381b05dda2d5dd8a432ae76db90a32f62ae42949
4rwill <christian10will@gmail.com>
2024-05-30 00:09:46 -0300

    Controle de Fluxo

---
+++ dio-trilha-java-basico/controle-fluxo/src/ExemploFor.java
@@ -0,0 +1,10 @@
+// ExemploFor.java
+public class ExemploFor {
+    public static void main(String[] args) {
+        int carneirinhos = 1;
+        for( ; carneirinhos <=3; ) {
+            System.out.println(carneirinhos + " - Carneirinho(s)");
+            carneirinhos ++;
+        }
+    }
+}
