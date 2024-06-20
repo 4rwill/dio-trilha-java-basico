d5bd50c43c07fffe0b125c35eb877bc5282a563a
4rwill <christian10will@gmail.com>
2024-06-20 10:11:45 -0300

    Desafio livro

---
+++ dio-trilha-java-basico/collections-java/src/List/Pesquisa/Livro/Livro.java
@@ -0,0 +1,42 @@
+package List.Pesquisa.Livro;
+
+public class Livro {
+    //attributes
+    private String titulo;
+    private String autor;
+    private int anoPublicacao;
+
+    //constructors
+
+    public Livro(String titulo, String autor, int anoPublicacao) {
+        this.titulo = titulo;
+        this.autor = autor;
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    //getters
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    //toString
+
+
+    @Override
+    public String toString() {
+        return "Livro{" +
+                "titulo='" + titulo + '\'' +
+                ", autor='" + autor + '\'' +
+                ", anoPublicacao=" + anoPublicacao +
+                '}';
+    }
+}
